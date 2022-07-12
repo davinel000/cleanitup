d65e39d4612fdde5a3db4b22f95877ab1404935c
Slava Romanov <davinel000@gmail.com>
2022-07-12 23:08:28 +0200

    Flocking behavior scene

---
+++ Assets/Clean it up!/Birds/Editor/CompositeBehaviorEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CompositeBehavior))]
+
+public class CompositeBehaviorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+    }
+}
